47950df0728cf87bcfd642277692b6813978234a
Tyagi1999 <52347329+Tyagi1999@users.noreply.github.com>
2019-07-16 09:50:46 +0530

    Add files via upload

---
+++ Coding_Problem_Solution_9.py
@@ -0,0 +1,53 @@
+# asked by Google.
+# A unival tree (which stands for "universal value") is a tree where all nodes under it have the same value.
+# Given the root to a binary tree, count the number of unival subtrees.
+# For example, the following tree has 5 unival subtrees:
+'''
+   0
+  / \
+ 1   0
+    / \
+   1   0
+  / \
+ 1   1
+'''
+
+class Node(object):
+    def __init__(self,val=None,left=None,right=None,is_unival=False):
+        self.val=val
+        self.left=left
+        self.right=right
+        self.is_unival=is_unival
+
+def count_unival(node):
+    if node.left==node.right==None:
+        node.is_unival=True
+        return 1
+    left=right=0
+    if node.left!=None:
+        left=count_unival(node.left)
+    if node.right!=None:
+        right=count_unival(node.right)
+
+    if node.left and node.right:
+        if (node.left.is_unival and node.right.is_unival
+            and node.val==node.left.val==node.right.val):
+            node.is_unival=True
+            return left+right+1
+        else:
+            return left+right
+    else:
+        if ((not node.left and node.val == node.right.val and node.right.is_unival)
+            or (not node.right and node.val==node.left.val and node.left.is_unival)):
+            node.is_unival=True
+            return left+right+1
+        else:
+            return left+right
+
+def main():
+    root=Node('0',Node('1'),Node('0',Node('1',Node('1'),Node('1')),Node('0')))
+    print(count_unival(root))
+
+if __name__=='__main__':
+    main()
+         
